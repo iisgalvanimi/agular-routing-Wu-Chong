e70c1b7d9f17c38faf1c2cfd4181f3a3fbf6936b
Wu-Chong <wu.chong.lin@iisgalvanimi.edu.it>
2020-04-08 19:52:22 +0000

    3

--- AngularRouting/src/app/spotify.service.ts
+++ AngularRouting/src/app/spotify.service.ts
@@ -1,6 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,9 @@ export class SpotifyService {
   constructor(private http: HttpClient) { }
 
   searchTrack(query: string) {
+
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQCkBD6X0XG4dlR9VTJdBOAjcgjOGz0bXPEEgpKrG_4F_O-SUZQIfmjABZ-gM35D1RmUtDxk2ZIQ2TyEQwqu5BkyCl35n1qkfQIcSPb02df6LTnJMymFchxqdRIR0Qw32k-ydnfLsZQECdm1A_OjH4YuvStlFUqQZdoTzMcbsehm'
-    });
+    const headers = new HttpHeaders({Authorization: environment.oauthToken});
 
     let obsTracks = this.http.get(url, { headers });
     return obsTracks;
--- AngularRouting/src/environments/environment.ts
+++ AngularRouting/src/environments/environment.ts
@@ -1,16 +1,4 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  oauthToken: "Bearer BQCuuM-DLmPw7RQIXkiSYyWEL-Ecf7aH_gA1-8kQyljnSeyk_F83aJ9CjcrfpIiTMCOIKtc7Vm8oWljKRpRC-7gwVtV3Fr7gjB5utHn-pyFOY85ErEvlm3IX0iFzupqAF5gchzUNj3oOHsxvjZhVhhc7Gelf5V3sPJrShkko-iql"
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
